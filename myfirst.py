7cc014203e75cf1e18d3f7b654e66c622810ce89
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-10-22 13:39:05 +0300

    Adding some new learni g scripts

--- myfirst.py
+++ myfirst.py
@@ -232,17 +232,19 @@ new_line()
 txt = " \"Philipians 1:3\" I thank my God anytime I remember You"
 print(txt)
 
-print("How old are you?", end= ' ')
-age = input()
-print("How tall are you?", end=' ')
-height = input()
-print("How much do you weigh?", end=' ')
-weight = input()
+# print("How old are you?", end= ' ')
+# age = input()
+# print("How tall are you?", end=' ')
+# height = input()
+# print("How much do you weigh?", end=' ')
+# weight = input()
 
-print(f"So you're {age} old, {height} tall and {weight} heavy.")
+# print(f"So you're {age} old, {height} tall and {weight} heavy.")
 
+y = input("Name? ")
 
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
 
-
-
-
+print(f"So {y} you're {age} old, {height} tall and {weight} ")
