0e0bb9bad211384a4483b2dace88e7f6db7468ae
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-10-24 12:22:48 +0300

    Added a rock paper scissor game

--- book.py
+++ book.py
@@ -99,13 +99,93 @@
 #     print(i)
 
 # importing modules
-import random 
-for i in range(5):
-    print(random.randint(1,10))
+# import random 
+# for i in range(5):
+#     print(random.randint(1,10))
     
-import sys
-while True:
-    response = input('Type exit to exit: ')
-    if response == 'exit':
-        sys.exit()
-    print('You typed ' + response + '.')
+# import sys
+# while True:
+#     response = input('Type exit to exit: ')
+#     if response == 'exit':
+#         sys.exit()
+#     print('You typed ' + response + '.')
+
+# import random 
+# secretNumber = random.randint(1, 20)
+# print( 'I am thinking of a number between 1 and 20')
+
+# for guessesTaken in range(1, 7):
+#     guess = int(input('Take a guess. '))
+    
+#     if guess < secretNumber:
+#         print('Your guess is too low!')
+#     elif guess > secretNumber:
+#         print('Your guess is too high!!')
+#     else:
+#         break
+    
+#     if guess == secretNumber:
+#         print('Good job! You guessed my number in' + str(guessesTaken) + 'guesses!')
+#     else:
+#         print('Nope.The number I was thinking of was ' + str(secretNumber) )
+        
+# Rock,Paper,Scissor
+import random, sys
+print('ROCK, PAPER, SCISSORS')
+
+wins = 0
+losses = 0
+ties = 0
+
+while True: #The main game loop
+    print('%s Wins, %s Losses, %s Ties' %(wins, losses, ties)) 
+    while True: # the player loop
+       playerMove =  input('Enter your move: (r)ock (p)aper (s)cissors or (q)uit') 
+       if playerMove == 'q':
+           sys.exit()
+       if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
+           break
+       print('Type one of r, p, s, or q.')
+    #    Display what the player choose
+       if playerMove == 'r':
+           print('ROCK versus...')
+       elif playerMove == 'p':
+           print('PAPER versus...')
+       elif playerMove == 's':
+           print('SCISSORS versus...')
+    # Display what computer choose
+       randomNumber = random.randint(1, 3)
+       if randomNumber == 1:
+           computerMove = 'r'
+           print('ROCK')
+       elif randomNumber == 2:
+           computerMove = 'p'
+           print('PAPER')
+       elif randomNumber == 3:
+           computerMove = 's'
+           print('SCISSORS')
+     #Display and record the win/loss/tie:
+       if playerMove == computerMove:
+         print('It is a tie!')
+         ties = ties + 1
+       elif playerMove == 'r' and computerMove == 's':
+           print('You Win!')
+           wins = wins + 1
+       elif playerMove == 'p' and computerMove == 'r':
+           print('You Win!')
+           wins = wins + 1
+       elif playerMove == 's' and computerMove == 'p':
+           print('You Win!')
+           wins = wins + 1  
+       elif playerMove == 'r' and computerMove == 'p':
+           print('You Lose!')
+           losses = losses + 1   
+       elif playerMove == 'p' and computerMove == 's':
+           print('You Lose!')
+           losses = losses + 1  
+       elif playerMove == 's' and computerMove == 'r':
+           print('You Loose!')
+           losses = losses + 1       
+            
+               
+                      
