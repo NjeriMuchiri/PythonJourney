4aa4d933eb72542ed4e0684dba4c21f69c3679e7
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-11-03 09:18:52 +0300

    Added the not in keywords

--- book.py
+++ book.py
@@ -317,8 +317,15 @@
 # for i in [0,1,2,3]:
 #     print(i)
 
-supplies = ['pens', 'staples', 'flame-throwers', 'binders']
-for i in range(len(supplies)):
-    print('Index ' + str(i) + ' in supplies: ' + supplies[i])
+# supplies = ['pens', 'staples', 'flame-throwers', 'binders']
+# for i in range(len(supplies)):
+#     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
 
-
+myPets = ['Zophie', 'Pooka', 'FatTail']
+name = input('Enter a pet name: ')
+if name not in myPets:
+    print('I do not have a pet named ' + name)
+else:
+    print(name + ' is my pet.')
+    
+    
