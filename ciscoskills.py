87271ae5b1615cd0a95e4ccf7450076a9743d8f7
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-11-23 12:56:47 +0300

    Added a program that calculate leap year and a common year

--- ciscoskills.py
+++ ciscoskills.py
@@ -5,11 +5,26 @@
 # y = int(input())
 # print(x + y)
 
-bestPlant = str(input("Name one of the best and helpful inhouse plant: "))
+# bestPlant = str(input("Name one of the best and helpful inhouse plant: "))
 
-if bestPlant == "Spathiphyllum":
-    print("Yes - Spathiphyllum is the best plant ever!!")
-if bestPlant == "spathiphyllum":
-    print("No, I want a big Spathiphyllum!")
-elif bestPlant != "Spathiphyllum":
-    print("Spathyphyllum, not", bestPlant)
+# if bestPlant == "Spathiphyllum":
+#     print("Yes - Spathiphyllum is the best plant ever!!")
+# elif bestPlant == "spathiphyllum":
+#     print("No, I want a big Spathiphyllum!")
+# else:
+#     print("Spathyphyllum, not", bestPlant)
+
+# Leap year and common year
+year = int(input("Enter a year: "))
+
+if year < 1582:
+	print("Not within the Gregorian calendar period")
+else:
+	if year % 4 != 0:
+		print(year,"Common year")
+	elif year % 100 != 0:
+		print(year, "Leap year")
+	elif year % 400 != 0:
+		print(year, "Common year")
+	else:
+		print(year, "Leap year")
