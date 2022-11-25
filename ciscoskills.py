83b0a9f8630cf6860d2c6d2358814283e019e928
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-11-25 14:21:38 +0300

    Adding new sorting program

--- ciscoskills.py
+++ ciscoskills.py
@@ -15,16 +15,36 @@
 #     print("Spathyphyllum, not", bestPlant)
 
 # Leap year and common year
-year = int(input("Enter a year: "))
+# year = int(input("Enter a year: "))
 
-if year < 1582:
-	print("Not within the Gregorian calendar period")
-else:
-	if year % 4 != 0:
-		print(year,"Common year")
-	elif year % 100 != 0:
-		print(year, "Leap year")
-	elif year % 400 != 0:
-		print(year, "Common year")
-	else:
-		print(year, "Leap year")
+# if year < 1582:
+# 	print("Not within the Gregorian calendar period")
+# else:
+# 	if year % 4 != 0:
+# 		print(year,"Common year")
+# 	elif year % 100 != 0:
+# 		print(year, "Leap year")
+# 	elif year % 400 != 0:
+# 		print(year, "Common year")
+# 	else:
+# 		print(year, "Leap year")
+  
+# import time
+
+# for count in range(1,6):
+#     print(count, "Mississippi")
+#     time.sleep(1)
+
+# print("Ready or not, here I come")
+
+##Sorting right order
+my_list = [8, 10, 6, 2, 4]  # list to sort
+swapped = True # It's a little fake, we need it to enter the while loop.
+while swapped:
+   swapped = False # no swaps so far
+   for i in range(len(my_list) - 1):
+       if my_list[i] > my_list[i + 1]:
+           swapped = True # a swap occurred!
+           my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print(my_list)
