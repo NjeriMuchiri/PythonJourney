7907c48723357b47c6dcce40b6541ac291aea386
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-01 16:41:11 +0300

    Added a new program from skills

--- ciscoskills.py
+++ ciscoskills.py
@@ -51,14 +51,81 @@
 
 
 # BMI
-def bmi(weight,height):
-    weight = float(input("Enter your weight: "))
-    height = float(input("Enter your height: "))
-    if height < 0.5 or weight > 300:
-        return None
-    calculations = weight / height **2
-    return calculations
-print("your bmi is: ",bmi(144,1.7))
+# def bmi(weight,height):
+#     weight = float(input("Enter your weight: "))
+#     height = float(input("Enter your height: "))
+#     if height < 0.5 or weight > 300:
+#         return None
+#     calculations = weight / height **2
+#     return calculations
+# print("your bmi is: ",bmi(144,1.7))
+
+# factorial
+# def factorial_function(n):
+#     if n < 0:
+#         return None
+#     if n < 2:
+#         return 1
+#     product = 1
+#     for i in range(2, n + 1):
+#         product *= i
+#     return product
+# for n in range(1, 10):
+#     print(n, factorial_function(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
