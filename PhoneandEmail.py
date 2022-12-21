d9644a00a1da626d7187fedaecf6c35214325397
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-21 17:15:58 +0300

    Added some new codes with pyperclip module

--- PhoneandEmail.py
+++ PhoneandEmail.py
@@ -8,3 +8,21 @@ phoneRegex = re.compile(r'''(
     (\d{4})
     (\s*(ext|X|ext.)\S*(\d{2,5}))?
 )''', re.VERBOSE)
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+   #username
+    @                   #symbol
+    [a-zA-Z0-9.-]+      #domain name
+    (\.[a-zA-Z]{2,4})   #dot-something
+    )''', re.VERBOSE)
+
+text = str(pyperclip.paste())
+
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+        matches.append(phoneNum)
+    for groups in emailRegex.findall(text):
+        matches.append(groups[0])
