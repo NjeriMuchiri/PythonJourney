215b37af19da6bd6336b32b45b3ca04ab56595c6
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-21 11:44:52 +0300

    Added a new phone and email finder on the clipboard program

---
+++ PhoneandEmail.py
@@ -0,0 +1,10 @@
+import pyperclip,re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?
+    (\s|-|\.)?
+    (\d{3})
+    (\s|-|\.)
+    (\d{4})
+    (\s*(ext|X|ext.)\S*(\d{2,5}))?
+)''', re.VERBOSE)
