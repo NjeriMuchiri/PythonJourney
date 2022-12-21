124ca3aa2e77f6af547b82ebfbcfe9a3ccca397f
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-12-21 21:56:26 +0300

    Writing files

--- book.py
+++ book.py
@@ -476,6 +476,13 @@
 baconFile = open('bacon.txt', 'w')
 baconFile.write('Hello World!\n')
 baconFile.close()  
+baconFile = open('bacon.txt', 'a')
+baconFile.write('Bacon is not a vegetble.')
+baconFile.close()
+baconFile = open('bacon.txt')
+content = baconFile.read()
+baconFile.close()
+print(content)
 
 
 
