566c76ec8f5746bc9f94c8a9e49db9fe10216a9c
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-04 11:12:43 +0300

    Added a new program small one

--- tutorial.py
+++ tutorial.py
@@ -1,3 +1,16 @@
-currentYear = 2023
-answer = int(input('When is your birth year? '))
-print('You are', currentYear - answer, 'years old')
+# import random
+
+# people = ["Allen", "Michael", "John", "Ben"]
+# chosen_person = random.choice(people)
+# people.remove(chosen_person)
+# print(f'The chosen person is: {chosen_person}')
+# print(f'The length of people list is {len(people)}')
+
+friends = ["Allen","Ken","Ben", "John", "Michael","Jim"]
+bestFriend = "Ken"
+if bestFriend in friends:
+    print(f"{bestFriend} my best of friends is here!")
+# for friend in friends:
+#     if friend == bestFriend:
+#       print(f"{bestFriend}'s here")
+
