0cbcde7c418f54d1f81c03090aba48ccc37de0f8
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-04 12:32:30 +0300

    Added a program that elevates the user input

--- tutorial.py
+++ tutorial.py
@@ -6,11 +6,23 @@
 # print(f'The chosen person is: {chosen_person}')
 # print(f'The length of people list is {len(people)}')
 
-friends = ["Allen","Ken","Ben", "John", "Michael","Jim"]
-bestFriend = "Ken"
-if bestFriend in friends:
-    print(f"{bestFriend} my best of friends is here!")
+# friends = ["Ann","Ken","Ben", "John", "Michael","Jim"]
+# bestFriend = "Ken"
+# # if bestFriend in friends:
+# #     print(f"{bestFriend} my best of friends is here!")
 # for friend in friends:
 #     if friend == bestFriend:
 #       print(f"{bestFriend}'s here")
+#       break
+#     else:
+#         print(f"This is {friend}")
+#     print("Defaulting")
 
+while True:
+    number = input("Please type something: ")
+    if number == 'out':
+        break
+    elif int(number):
+        print(f"You are on a {number} floor")
+    else:
+        print("Type out to break out of the loop")
