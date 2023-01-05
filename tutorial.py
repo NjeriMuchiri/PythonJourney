61c4c2350f93d6d0e0e54b8d366b65459331d610
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-05 10:37:54 +0300

    made a program that hides credit card credentials

--- tutorial.py
+++ tutorial.py
@@ -18,11 +18,34 @@
 #         print(f"This is {friend}")
 #     print("Defaulting")
 
-while True:
-    number = input("Please type something: ")
-    if number == 'out':
-        break
-    elif int(number):
-        print(f"You are on a {number} floor")
-    else:
-        print("Type out to break out of the loop")
+# while True:
+#     number = input("Please type something: ")
+#     if number == 'out':
+#         print("Outing of here!")
+#         break
+#     elif int(number):
+#         print(f"You are on a {number} floor")
+#     else:
+#         print("Type out to break out of the loop")
+
+# def doPyth(a,b,c):
+#        num_ab = a ** 2 + b ** 2
+#        num_c = c ** 2
+#        if num_ab == num_c:
+#            print(f"The combination of: {a}, {b}, {c} supports pyth law")
+#        else:
+#             print(f"The combination of: {a}, {b},  {c} does not support pyth law!")
+# doPyth(3,4,5) 
+# doPyth(5,6,7)      
+
+def hide_cards(card_numbers):
+    cards_list = []
+    for card_number in card_numbers:
+        last_four_digits = card_number[-4:]
+        formattedCreditCard =  f'**** **** **** {last_four_digits}'
+        cards_list.append(formattedCreditCard)
+        
+    return cards_list
+    
+my_card = hide_cards(['1234567890123456','3456654378967654','2345543267898765'])
+print(my_card)
