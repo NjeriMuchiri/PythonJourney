f5e6916c79e6667e61ebf1566f3249c50243bd46
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-08 22:05:46 +0300

    Added a csv file for habdling my items list

--- opp1.py
+++ opp1.py
@@ -36,6 +36,7 @@
 # print(course.get_average_grade())     
 
 # oop2
+import csv
 class Item:
     pay_rate = 0.8 #pay rate after 20% discount
     all = []
@@ -58,16 +59,32 @@ class Item:
     
     def apply_discount(self):
         self.price = self.price * self.pay_rate
+    
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r')as f:
+            reader = csv.DictReader(f) #reads content as a dictionary
+            items = list(reader)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+         )
+    @staticmethod         
+    def is_integer(num):
+        #we will count out the floats that are point zeroi.e 10.0 etc
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+        
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
        
-item1 = Item('Iphone',1000,5)
-item2 = Item('laptop',10000,2)
-item3 = Item('cable',100,4)
-item4 = Item('Mouse',160,3)
-item5= Item('keyboard',150,4)
-
-print(Item.all)
+print(Item.is_integer(7.0))
 # print(Item.__dict__) #All the attribute for class level
 # print(item2.__dict__)#All the attribute for instance level
 
