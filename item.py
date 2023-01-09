e47ff2286e261bd46c527dc6f4ccfc0a30c4f262
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-09 11:40:38 +0300

    Added a new file for handling the phone class

--- item.py
+++ item.py
@@ -46,22 +46,7 @@ class Item:
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
     
-class Phone(Item):
-         def __init__(self, name: str, price: float, quantity = 0,broken_phones = 0):
-         #call to super function to have acccess to all methods
-              super().__init__(
-                  name,price,quantity
-               )
-        #Run validations to the received arguments
-              assert broken_phones >= 0
-        #Assign to self object
-              self.broken_phones = broken_phones
-              
-              
-phone1 = Phone("Iphone13", 500, 5, 1)
 
-print(Phone.all)
-print(Item.all)
 
        
 # print(Item.is_integer(7.0))
---
+++ phone.py
@@ -0,0 +1,16 @@
+class Phone(Item):
+         def __init__(self, name: str, price: float, quantity = 0,broken_phones = 0):
+         #call to super function to have acccess to all methods
+              super().__init__(
+                  name,price,quantity
+               )
+        #Run validations to the received arguments
+              assert broken_phones >= 0
+        #Assign to self object
+              self.broken_phones = broken_phones
+              
+              
+phone1 = Phone("Iphone13", 500, 5, 1)
+
+print(Phone.all)
+print(Item.all)
