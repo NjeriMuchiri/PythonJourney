98a6d18550df578e8ac9a2cbf3804f1373432c51
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-31 15:35:43 +0300

    Added two new challenges

--- others.py
+++ others.py
@@ -144,15 +144,15 @@ class Solution:
         i = 0
         j = len(A) - 1
         
-        ans = -1
+        S = -1
         
         while i < j:
             if A[i] + A[j] < K:
-                ans = max(ans, A[i] + A[j])
+                S = max(S, A[i] + A[j])
                 i += 1
             else:
                 j -= 1
-        return ans
+        return S
 sum1 = Solution()
 print(sum1.twoSumlessThank([34,23,1,24,75,33,54,8], 60))           
 
