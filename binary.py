54cc78591ae7b16e02a991201693982619ef7a48
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-04 22:00:10 +0300

    binary tings

---
+++ binary.py
@@ -0,0 +1,17 @@
+#Define a tree
+#Adding a node
+#Building up a tree
+#printing all nodes in order
+#printing all nodes in Postorder
+#printing all nodes in Preorder
+#Breadth First search(bfs)-Algorithm using queues
+#Depth first search(dfs) - Algoithm using stacks
+#DFS Python code
+#Searching for a key in tree
+#Exercise - Tree top view
+
+class Node:
+    def __init__(self,data):
+        self.left = None
+        self.right = None
+        self.data = data
