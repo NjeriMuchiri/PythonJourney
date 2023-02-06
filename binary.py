51c119fd3266c1bad4bac4c1cb47a37cb1009fb7
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-06 10:54:45 +0300

    inserting nodes

--- binary.py
+++ binary.py
@@ -1,5 +1,5 @@
 #Define a tree
-#Adding a node
+#Define an insert function  
 #Building up a tree
 #printing all nodes in order
 #printing all nodes in Postorder
@@ -15,3 +15,51 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
+        
+    def insert(self,data):
+        if self.data is None:
+            self.data = data
+        else:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+
+def inOrderPrint(r):
+    if r is None:
+        return
+    else:
+        inOrderPrint(r.left)
+        print(r.data, end=' ')
+        inOrderPrint(r.right)
+
+def preOrderPrint(r):
+    if r is None:
+        return
+    else:
+        print(r.data, end=' ')
+        preOrderPrint(r.left)
+        preOrderPrint(r.right)
+        
+if __name__ == '__main__':                 
+    root = Node('g')
+    root.insert('c')
+    root.insert('b')
+    root.insert('a')
+    root.insert('e')
+    root.insert('d')
+    root.insert('f')
+    root.insert('i')
+    root.insert('h')
+    root.insert('j')
+    root.insert('k')
+    
+#print all nodes inorder
+preOrderPrint(root)
+    
