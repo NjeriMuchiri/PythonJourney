a731ecc69bf3c00ba6c4892f22562c4c64263f64
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-19 14:29:09 +0300

    added a new program

--- kashee.py
+++ kashee.py
@@ -1,5 +1,15 @@
-name = input("Please enter you name gal: ")
-if name == 'Kashee':
-    print("Hi Kashee")
+#program to print my name
+# name = input("Please enter you name gal: ")
+# if name == 'Kashee':
+#     print("Hi Kashee")
+# else:
+#     print("hi", name)
+
+#program to check whether the number is odd or even
+number = int(input("Please enter a number: "))
+if number % 2 == 1:
+    print(number," is odd ")
+elif number % 2 == 0:
+    print(number, "is even")
 else:
-    print("hi", name)
+    print("just a number")
