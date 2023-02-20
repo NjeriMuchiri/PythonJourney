0f6ed1e111000f7d5d80a2fa06789da8f982333e
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-20 16:05:38 +0300

    Added a new program

--- kashee.py
+++ kashee.py
@@ -6,10 +6,21 @@
 #     print("hi", name)
 
 #program to check whether the number is odd or even
-number = int(input("Please enter a number: "))
-if number % 2 == 1:
-    print(number," is odd ")
-elif number % 2 == 0:
-    print(number, "is even")
+# number = int(input("Please enter a number: "))
+# if number % 2 == 1:
+#     print(number," is odd ")
+# elif number % 2 == 0:
+#     print(number, "is even")
+# else:
+#     print("just a number")
+
+#Program to print maximum of two numbers
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+
+if num1 > num2:
+    print(num1, "is greater")
+elif num2 > num1:
+    print(num2, "is greater")
 else:
-    print("just a number")
+    print("they are equal")
